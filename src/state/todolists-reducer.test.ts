79dc6b62aa5fe527da8bafafa0ebe2148ec4812b
Victor <pobozhny94@gmail.com>
2024-01-03 23:17:13 +0300

    lesson 9. reducers, tests fixed bug

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -34,7 +34,7 @@ test('correct todolist should be added', () => {
         {id: todolistId2, title: "What to buy", filter: "all"}
     ]
 
-    const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle))
+    const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle, v1()))
 
     expect(endState.length).toBe(3);
     expect(endState[2].title).toBe(newTodolistTitle);
